0bf65d69c569f2def16d0b6f702ac7ce4a99721c
mktwohy <69822801+mktwohy@users.noreply.github.com>
2022-12-12 00:24:14 -0600

    use local JSON file instead of STAPI - STAPI URLs use HTTP, so they cannot be accessed on GitHub pages

--- main.js
+++ main.js
@@ -1,29 +1,24 @@
-function pickRandomEpisode() {
-    getJSON('http://stapi.co/api/v1/rest/episode/search')
-        .then(allEpisodes => {
-            let episode = allEpisodes.episodes.random()
-            $('#episode').text(`${episode.season.title}: ${episode.title}`)
+let episodes
+
+$(document).ready(() => {
+    $.getJSON('episodes.json')
+        .then((data) => {
+            episodes = data
+            pickRandomEpisode()
         })
+})
+
+
+function pickRandomEpisode() {
+    if (episodes === undefined) {
+        console.error("Cannot pick random episode; episodes have not been read from file yet.")
+        return
+    }
+    let episode = episodes.random()
+    console.log("Randomly picked episode: ", episode)
+    $('#episode').text(`${episode.season.title}: ${episode.title}`)
 }
 
-function getJSON(url) {
-    return new Promise((resolve, reject) => {
-        let req = new XMLHttpRequest()
-        req.onreadystatechange = () => {
-            if (req.readyState === 4) {
-                if (req.status === 200) {
-                    resolve(JSON.parse(req.response))
-                }
-                else {
-                    reject(req.response)
-                }
-            }
-        }
-        req.open('GET', url, true)
-        req.send()
-    })
-}
-
-Array.prototype.random = function() {
+Array.prototype.random = function () {
     return this[Math.floor(Math.random() * this.length)]
 }
